43752cd8fb25e93864d1184de224a21ac6b9661c
Amgad Malik <amgadmalik@yahoo.com>
2019-10-02 00:47:27 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+require 'pry'
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+  end 
+  
+  def self.count
+    @@count 
+  end 
+  
+  def self.genres 
+    genres_hash = {}
+    @@genres.each do |genre_type|
+      if genres_hash[genre_type]
+        genres_hash[genre_type] += 1 
+      else
+        genres_hash[genre_type] = 1
+      end 
+    end 
+    #binding.pry
+  end 
+  
+  def self.artists
+    
+  end 
+  
+  def self.genre_count
+    
+  end 
+end 
